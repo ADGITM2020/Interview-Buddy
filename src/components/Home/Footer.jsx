e8ad8ee76369ea85f58108165d1c1ceb02fc38d8
pankhuri-deshmukh <pankhurideshmukh1000@gmail.com>
2023-07-17 15:40:23 +0530

    Added navbar and footer

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsFillMicFill, BsFillMicMuteFill, BsFillCameraVideoFill, BsFillCameraVideoOffFill} from 'react-icons/bs';
+import {MdOutlineScreenShare, MdOutlineStopScreenShare, MdCallEnd} from 'react-icons/md';
 
 const Footer = () => {
+  const [mike, setMike] = useState(false);
+  const [cam, setCam] = useState(false);
+  const [sc, isSc] = useState(false);
+
+  const handleMike = () => {
+    setMike(!mike);
+  };
+  const handleCam = () => {
+    setCam(!cam);
+  };
+
+  const handleSc = () => {
+    isSc(!sc);
+  };
+
   return (
-    <div>
-      
+    <div className='w-full fixed bottom-0 h-10 bg-[#F3F3F3]'>
+      <ul className='flex justify-center'>
+        <li onClick={handleMike} className='flex ml-2 mr-2 mt-1'>
+        {mike ? <BsFillMicMuteFill size={30} className='p-1 rounded-full bg-red-300'/> : <BsFillMicFill size={30}className='p-1 rounded-full bg-blue-300'/>}
+        </li>
+        <li onClick={handleCam} className='flex ml-2 mr-2 mt-1'>
+        {cam ? <BsFillCameraVideoOffFill size={30} className='p-1 rounded-full bg-red-300'/> : <BsFillCameraVideoFill size={30} className='p-1 rounded-full bg-blue-300'/>}
+        </li>
+        <li onClick={handleSc} className='flex ml-2 mr-2 mt-1'>
+        {sc ? <MdOutlineScreenShare size={30} className='p-1 rounded-full bg-blue-300'/>: <MdOutlineStopScreenShare size={30} className='p-1 rounded-full bg-slate-200'/> }
+        </li>
+        <li className='flex ml-2 mr-2 mt-0'>
+          <MdCallEnd size={40} className='pl-1 pr-1 bg-red-500 border-y-4 rounded-2xl'/>
+        </li>
+
+      </ul>
+
     </div>
   )
 }
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import {AiOutlineUserAdd, AiOutlineUser, AiOutlineSetting, AiOutlineFieldTime} from 'react-icons/ai';
+
 
 const Navbar = () => {
-  return (
-    <div>
-      
+
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (    
+  <div className=' h-10 w-full bg-[#f3f3f3]'>
+
+      <ul className='hidden sm:flex justify-end'>
+        <li className='flex p-1 ml-2 mr-2 mt-1'><AiOutlineFieldTime className='w-6 h-6'/></li>
+        <li className='flex p-1 ml-2 mr-2 mt-1'><AiOutlineSetting className='w-6 h-6'/></li>
+        <li className='flex p-1 ml-2 mr-2 mt-1 bg-blue-300 rounded-full'><AiOutlineUser className='w-6 h-6'/></li>
+        <li className='flex p-1 ml-2 mr-2 mt-1  bg-[#d3d3d3] rounded-lg'><AiOutlineUserAdd className='w-6 h-6'/> Add</li>
+      </ul>
+
+    <div onClick={handleNav} className='block sm:hidden'>
+    <AiOutlineMenu className='h-6 w-6 ml-2' />
     </div>
+
+    <div className={nav ? 'fixed top-0 left-0 pt-4 w-[30%] h-full ease-in-out duration-500 bg-white sm:hidden' : ' ease-in duration-500 fixed left-[-100%]'}> 
+
+      <div onClick={handleNav} className='flex justify-end'>
+      <AiOutlineClose size={20} className='mr-2'/>
+      </div>
+
+      <ul className='pl-2 pr-2'>
+      <li className='border-b border-gray-300 py-2'>My Account</li>
+      <li className='border-b border-gray-300 py-2'>Settings</li>
+      <li className='border-b border-gray-300 py-2'>Timer</li>
+      <li className='border-b border-gray-300 py-2'>Add Member</li>
+      </ul>
+    </div>
+      
+  </div>
   )
 }
 
