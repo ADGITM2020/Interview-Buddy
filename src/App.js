d1f5b7dc1281d2c2ee86e840810cec1d42a7b1fa
Dhruvgera <dhruvgera61@gmail.com>
2023-07-19 01:23:18 +0530

    IB: Add video and audio sharing
    TOFIX: Screensharing partially broken
    Signed-off-by: Dhruvgera <dhruvgera61@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Navbar from './components/Home/Navbar';
 import Footer from './components/Home/Footer';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import CreateRoom from "./CreateRoom";
+import Room from "./Room";
 
 function App() {
   return (
-  <>
-    <Navbar/>
-    <Footer/>
-  </>
+    <>
+        <Navbar/>
+      <div className="App">
+        <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={CreateRoom} />
+          <Route path="/room/:roomID" component={Room} />
+        </Switch>
+        </BrowserRouter>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
